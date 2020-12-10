8253c55324762e466106ed83b138844bdffbe7a7
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-10 15:47:37 -0600

    Adds password checking and comments

--- src/capstone/Capstone.java
+++ src/capstone/Capstone.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package capstone;
 
 import javafx.application.Application;
@@ -13,7 +8,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author tmgoo
+ * @author Tyler Goodfred
  */
 public class Capstone extends Application {
     
