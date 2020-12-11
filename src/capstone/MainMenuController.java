cce9bcc65205bb18c1c68af9b396a391913437b7
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-11 03:33:32 -0600

    main menu adding features currently

--- src/capstone/MainMenuController.java
+++ src/capstone/MainMenuController.java
@@ -6,22 +6,102 @@
 package capstone;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 
 /**
  * FXML Controller class
  *
- * @author tmgoo
+ * @author Tyler Goodfred
  */
 public class MainMenuController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    TableColumn allTitleTab;
+    @FXML
+    TableColumn allAuthTab;
+    @FXML
+    TableColumn allDescTab;
+    @FXML
+    TableColumn allPgCntTab;
+    @FXML
+    TableColumn allGenreTab;
+    @FXML
+    TableColumn allRateTab;
+    @FXML
+    TableColumn readTitleTab;
+    @FXML
+    TableColumn readAuthTab;
+    @FXML
+    TableColumn readDescTab;
+    @FXML
+    TableColumn readPgCntTab;
+    @FXML
+    TableColumn readGenreTab;
+    @FXML
+    TableColumn readRateTab;
+    @FXML
+    TableColumn searchTitleTab;
+    @FXML
+    TableColumn searchAuthTab;
+    @FXML
+    TableColumn searchDescTab;
+    @FXML
+    TableColumn searchPgCntTab;
+    @FXML
+    TableColumn searchGenreTab;
+    @FXML
+    TableColumn searchRateTab;
+    @FXML
+    TableColumn recTitleTab;
+    @FXML
+    TableColumn recAuthTab;
+    @FXML
+    TableColumn recDescTab;
+    @FXML
+    TableColumn recPgCntTab;
+    @FXML
+    TableColumn recGenreTab;
+    @FXML
+    TableColumn recRateTab;
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Connection conn = null;
+        try {
+            String url2 = "jdbc:mysql://localhost:3306/capstone?zeroDateTimeBehavior=CONVERT_TO_NULL";
+            conn = DriverManager.getConnection(url2, "root", "Rootpass1");
+            Statement stmt = null;
+            String query = "SELECT bookID,bookTitle,bookDescription,bookPageCount,bookRating,bookTotalReads,mainGenre FROM capstone.books"; //gets login data from database
+            try {
+                stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()){
+                    //usernameList.add(rs.getString(1));  //adds usernames to username list
+                    //passwordList.add(rs.getString(2));  //adds passwords to password list
+                }
+            }
+            catch (SQLException e ) {
+              throw new Error("Problem", e);
+            } finally {
+              if (stmt != null) { stmt.close(); }
+            }
+        }
+        catch (SQLException e) {
+            throw new Error("Problem", e);
+        } 
+        finally {
+          try {if (conn != null) {conn.close();}} 
+          catch (SQLException ex) {System.out.println(ex.getMessage());}
+        }
     }    
     
 }
