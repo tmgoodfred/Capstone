2644da2e7b83bfb76ec8fb0f8ad40f191c6e2627
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-13 19:37:00 -0600

    added back button function

--- src/capstone/MoreBookInfoController.java
+++ src/capstone/MoreBookInfoController.java
@@ -7,7 +7,14 @@ package capstone;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,10 +23,19 @@ import javafx.fxml.Initializable;
  */
 public class MoreBookInfoController implements Initializable {
 
+    @FXML
+    Button backBtn;
+    
     static int bookID = MainMenuController.bookIDtoShare;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println(bookID);
-    }    
+    }  
+    
+    @FXML
+    private void backButtonAction(ActionEvent event) throws Exception { //back button reinitializes and launches the main login screen
+        Stage stage2 = (Stage) backBtn.getScene().getWindow();
+        stage2.close();
+    }
     
 }
