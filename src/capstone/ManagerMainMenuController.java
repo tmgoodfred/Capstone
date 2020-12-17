33de823a75f1a96776808c1950d62f0c25abc573
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-16 20:53:19 -0600

    added function of add button

--- src/capstone/ManagerMainMenuController.java
+++ src/capstone/ManagerMainMenuController.java
@@ -2,6 +2,7 @@ package capstone;
 
 import capstone.User;
 import capstone.Book;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,11 +10,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -49,6 +56,18 @@ public class ManagerMainMenuController implements Initializable {
     TableColumn allGenreTab;
     @FXML
     TableColumn allRateTab;
+    
+    @FXML
+    Button addBookButton;
+    
+    @FXML
+    public void addButtonAction(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("AddBook.fxml"));
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
