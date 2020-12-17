e8e27bf2f475d2ddba08a6ac9400fe03e13d89bd
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-16 21:31:47 -0600

    removal of imports

--- src/capstone/AddBookController.java
+++ src/capstone/AddBookController.java
@@ -1,7 +1,6 @@
 package capstone;
 
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -26,7 +25,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.WritableImage;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
