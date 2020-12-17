3447e57cb5f0d0cd47be4ee547bd4bd4906033de
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-16 18:19:27 -0600

    converted to include search and read functions

--- src/capstone/Book.java
+++ src/capstone/Book.java
@@ -25,9 +25,30 @@ public class Book {
     static List<Integer> bookTotalReads = new ArrayList<>();
     static List<String> bookMainGenre = new ArrayList<>();
     
-    private String bookTitles, bookAuthors, bookDescriptions, mainGenres;   //variables for the generic class
-    private Integer bookIDs, bookPageCounts;
-    private Double bookRatings;
+    static List<Integer> searchbookID = new ArrayList<>();  //lists to store book data to be added to the tableviews
+    static List<String> searchbookTitle = new ArrayList<>();
+    static List<String> searchbookAuthor = new ArrayList<>();
+    static List<String> searchbookDescription = new ArrayList<>();
+    static List<Integer> searchbookPageCount = new ArrayList<>();
+    static List<Double> searchbookRating = new ArrayList<>();
+    static List<Integer> searchbookTotalReads = new ArrayList<>();
+    static List<String> searchbookMainGenre = new ArrayList<>();
+    
+    static List<Integer> readbookID = new ArrayList<>();  //lists to store book data to be added to the tableviews
+    static List<String> readbookTitle = new ArrayList<>();
+    static List<String> readbookAuthor = new ArrayList<>();
+    static List<String> readbookDescription = new ArrayList<>();
+    static List<Integer> readbookPageCount = new ArrayList<>();
+    static List<Double> readbookRating = new ArrayList<>();
+    static List<Integer> readbookTotalReads = new ArrayList<>();
+    static List<String> readbookMainGenre = new ArrayList<>();
+    
+    //private String bookTitles, bookAuthors, bookDescriptions, mainGenres;   //variables for the generic class
+    //private Integer bookIDs, bookPageCounts;
+    //private Double bookRatings;
+    public String bookTitles, bookAuthors, bookDescriptions, mainGenres;   //variables for the generic class
+    public Integer bookIDs, bookPageCounts;
+    public Double bookRatings;
     
     public Book(){    //the generic class for books to be added to the tableviews
     this.bookIDs = 0;
@@ -77,5 +98,20 @@ public class Book {
             items.add(new Book(bookID.get(i), bookTitle.get(i), bookAuthor.get(i), bookDescription.get(i), bookPageCount.get(i), bookMainGenre.get(i),bookRating.get(i)));
         }
         return items;
+    }
+    
+    public static ObservableList<Book> getSearchItems(){ //gets specifically the items that match the searched term on the search tab into one observable list - needed for putting into a table column
+        ObservableList<Book> items2 = FXCollections.observableArrayList();
+        for(int i=0; i<searchbookTitle.size();i++){
+            items2.add(new Book(searchbookID.get(i), searchbookTitle.get(i), searchbookAuthor.get(i), searchbookDescription.get(i), searchbookPageCount.get(i), searchbookMainGenre.get(i),searchbookRating.get(i)));
+        }
+        return items2;
+    }
+    public static ObservableList<Book> getReadItems(){ //gets the items read by the users
+        ObservableList<Book> items3 = FXCollections.observableArrayList();
+        for(int i=0; i<readbookTitle.size();i++){
+            items3.add(new Book(readbookID.get(i), readbookTitle.get(i), readbookAuthor.get(i), readbookDescription.get(i), readbookPageCount.get(i), readbookMainGenre.get(i),readbookRating.get(i)));
+        }
+        return items3;
     }
 }
