d7b2759396f69747b2c8934461b9323ec775fccf
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-21 00:26:49 -0600

    change to remote database

--- src/capstone/BookSurveyController.java
+++ src/capstone/BookSurveyController.java
@@ -93,8 +93,8 @@ public class BookSurveyController implements Initializable {
 
                 Connection conn = null;
                 try {
-                    String url2 = "jdbc:mysql://localhost:3306/capstone?zeroDateTimeBehavior=CONVERT_TO_NULL";
-                    conn = DriverManager.getConnection(url2, "root", "Rootpass1!");
+                    String url2 = "jdbc:mysql://192.168.1.23:3306/capstone?zeroDateTimeBehavior=CONVERT_TO_NULL";
+                    conn = DriverManager.getConnection(url2, "tyler", "Rootpass1!");
                     Statement stmt = null;
                     String query = "SELECT bookRating, bookTotalReads FROM capstone.books WHERE bookID = "+MainMenuController.bookIDtoShare+";"; //gets relevant book data from database
                     try {
