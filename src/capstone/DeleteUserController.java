38caaa322c03faf41dc331c7f35b34aa6861e96f
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-20 23:22:24 -0600

    change root pass

--- src/capstone/DeleteUserController.java
+++ src/capstone/DeleteUserController.java
@@ -36,7 +36,7 @@ public class DeleteUserController implements Initializable {
         Connection conn = null;
             try {
                 String url2 = "jdbc:mysql://localhost:3306/capstone?zeroDateTimeBehavior=CONVERT_TO_NULL";
-                conn = DriverManager.getConnection(url2, "root", "Rootpass1");
+                conn = DriverManager.getConnection(url2, "root", "Rootpass1!");
                 Statement stmt = conn.createStatement();
                     try {
                         stmt.executeUpdate(delete);
