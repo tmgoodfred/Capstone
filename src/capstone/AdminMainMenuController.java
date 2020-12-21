be98a5ecc6bdf3163fe6476d5c79067ed751e5a6
Tyler Goodfred <tmgoodfred@gmail.com>
2020-12-21 01:19:35 -0600

    adding jython

--- src/capstone/AdminMainMenuController.java
+++ src/capstone/AdminMainMenuController.java
@@ -3,6 +3,7 @@ package capstone;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import org.python.util.PythonInterpreter;
 
 /**
  * FXML Controller class
@@ -14,6 +15,9 @@ public class AdminMainMenuController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Test of mobile stuff
+        try(PythonInterpreter pyInterp = new PythonInterpreter()) {
+      pyInterp.exec("print('Hello Python World!')");
+    }
     }    
     
 }
